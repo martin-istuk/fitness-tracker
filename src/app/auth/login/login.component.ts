0168bce634465ad51056ef3f2280bc947d9dea6e
martin-istuk <45366040+martin-istuk@users.noreply.github.com>
2022-05-19 13:48:45 +0200

    section 3 done

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {}
+
+  onLogin(form: NgForm) {
+    console.log(form);
+  }
+}
